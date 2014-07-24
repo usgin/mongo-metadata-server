712d7b31a16b0d7088d20c34401b8b4dd3ca4086
asonnenschein <adrian.sonnenschein@gmail.com>
2014-07-24 16:59:29 -0700

    refactored utils module

--- src/routes.js
+++ src/routes.js
@@ -9,218 +9,254 @@ var mongo = require('./mongo-config')
   , request = require('request')
   , xml2json = require('xml2json');
 
-module.exports = routes = {
-  // Text-based search for records
-  search: function (req, res, next) {
-    var opts = {
-      search_terms: req.searchTerms,
-      limit: req.limit,
-      skip: req.skip,
-      published_only: req.publishedOnly || false,
-      error: function (err) {
-        return next(new errors.DatabaseReadError(
-          'Error searching for documents'));
-      },
-      success: function (result) {
-        console.log('SEARCH FOR', req.searchTerms);
-        res.header('Content-Type', 'application/json');
-        return res.send(result);
-      }
-    };
-    return da.search(mongo.searchUrl, opts);
-  },
-  // List records or collections (as JSON)
-  listResources: function (req, res, next) {
-    var db = mongo.getDb(req.resourceType)
-      , opts = {
-          include_docs: true,
-          clean_docs: true,
-          error: function (err) {
-            return next(new errors.DatabaseReadError(
-              'Error searching for documents'));
-          },
-          success: function (result) {
-            console.log('GET ALL', req.resourceType, 's');
-          }
-        };
-    return da.listDocs(db, opts);
-  },
-  // List records in a specific format
-  viewRecords: function (req, res, next) {
-    var db = mongo.getDb('record');
-  },
-  // Create a new record or collection
-  newResource: function (req, res, next) {
+// Text-based search for records
+function search (req, res, next) {
+  var opts = {
+    search_terms: req.searchTerms,
+    limit: req.limit,
+    skip: req.skip,
+    published_only: req.publishedOnly || false,
+    error: function (err) {
+      return next(new errors.DatabaseReadError(
+        'Error searching for documents'));
+    },
+    success: function (result) {
+      console.log('SEARCH FOR', req.searchTerms);
+      res.header('Content-Type', 'application/json');
+      return res.send(result);
+    }
+  };
+  return da.search(mongo.searchUrl, opts);
+}
 
-  },
-  // Harvest an existing record
-  harvestRecord: function (req, res, next) {
-    if ((!req.url) || (!req.format)) {
-      return next(new errors.ArgumentError(
-        'Request did not supply the requisite arguments: url and format.'))
-    } else {
-      var opts = {uri: req.url};
-      request(opts, function (err, response, body) {
-        if (err) {
-         return next(new errors.RequestError(
-             'The given URL resulted in an error: ' + err));
+// List records or collections (as JSON)
+function listResources (req, res, next) {
+  var db = mongo.getDb(req.resourceType)
+    , opts = {
+        include_docs: true,
+        clean_docs: true,
+        error: function (err) {
+          return next(new errors.DatabaseReadError(
+            'Error searching for documents'));
+        },
+        success: function (result) {
+          console.log('GET ALL', req.resourceType, 's');
+        }
+      };
+  return da.listDocs(db, opts);
+}
+
+// List records in a specific format
+function viewRecords (req, res, next) {
+  var db = mongo.getDb('record');
+}
+
+// Create a new record or collection
+function newResource (req, res, next) {
+
+}
+
+// Harvest an existing record
+function harvestRecord (req, res, next) {
+  if ((!req.url) || (!req.format)) {
+    return next(new errors.ArgumentError(
+      'Request did not supply the requisite arguments: url and format.'))
+  } else {
+    var opts = {uri: req.url};
+    request(opts, function (err, response, body) {
+      if (err) {
+       return next(new errors.RequestError(
+           'The given URL resulted in an error: ' + err));
+      } else {
+        if (!utils.validateHarvestFormat(req.format, body)) {
+          return next(new errors.ValidationError(
+            'The document at the given URL did not match the format specified.'));
         } else {
-          if (!utils.validateHarvestFormat(req.format, body)) {
-            return next(new errors.ValidationError(
-              'The document at the given URL did not match the format specified.'));
+          if (req.format === 'csv') {
+            csv2json.readCSV(body, req, res, next);
           } else {
-            if (req.format === 'csv') {
-              csv2json.readCSV(body, req, res, next);
-            } else {
-              var data = xml2json.toJson(body, {object: true, reversible: true})
-                , entries;
-              switch (req.format) {
-                case 'atom.xml':
-                  var entry = data.feed.entry;
-                  if (_.isArray(entry)) {
-                    entries = entry;
-                  } else if (_.isObject(entry)) {
-                    entries = [entry];
-                  } else {
-                    entries = [];
-                  }
-                  break;
-                case 'iso.xml':
-                  entries = [data];
-                  break;
-                case 'fgdc.xml':
-                  entries = [data];
-                  break;
-              }
-              req.entries = entries;
-              return next();
+            var data = xml2json.toJson(body, {object: true, reversible: true})
+              , entries;
+            switch (req.format) {
+              case 'atom.xml':
+                var entry = data.feed.entry;
+                if (_.isArray(entry)) {
+                  entries = entry;
+                } else if (_.isObject(entry)) {
+                  entries = [entry];
+                } else {
+                  entries = [];
+                }
+                break;
+              case 'iso.xml':
+                entries = [data];
+                break;
+              case 'fgdc.xml':
+                entries = [data];
+                break;
             }
+            req.entries = entries;
+            return next();
           }
         }
-      })
-    }
-  },
-  // Upload an existing record
-  uploadRecord: function (req, res, next) {
-
-  },
-  // Put data in the database
-  saveRecord: function (req, res, next) {
-    var entries = req.entries
-      , db = mongo.getDb('harvest');
-    var opts = {
-      docs: entries,
-      error: function (err) {
-        return next(new errors.DatabaseWriteError(
-          'Error writing to the database'));
-      },
-      success: function (newHarvestDocs) {
-        var opts = {
-          design: 'input',
-          format: req.format,
-          keys: (function () {
-            var docIds = [];
-            _.each(newHarvestDocs, function (doc) {
-              docIds.push(doc.id);
-            });
-            return docIds;
-          })(),
-          error: function (err) {
-            return next(new errors.DatabaseReadError(
-              'Error reading document from database'));
-          },
-          success: function (transformedDocs) {
-            var db = mongo.getDb('record');
-            _.each(transformedDocs.row, function (doc) {
-              var harvestInfo = {
-                OriginalFormat: req.format,
-                HarvestURL: req.url,
-                HarvestDate: utils.getCurrentDate(),
-                HarvestRecordId: doc.id
-              };
-              _.extend(doc.value.HarvestInformation, harvestInfo);
-              _.extend(doc.value, {
-                Collections: req.collections || [],
-                ModifiedDate: utils.getCurrentDate()
-              })
-            });
-            var opts = {
-              docs: (function () {
-                var docVals = [];
-                _.each(transformedDocs.rows, function (doc) {
-                  docVals.push(doc.value);
-                });
-                return docVals;
-              })(),
-              error: function (err) {
-                return next(new errors.DatabaseWriteError(
-                  'Error writing to the database'));
-              },
-              success: function (newRecords) {
-                var resBody = (function () {
-                  var results = [];
-                  _.each(newRecords, function (rec) {
-                    results.push('/metadata/record/' + rec.id + '/');
-                  });
-                  return results;
-                })();
-                return res.send(resBody, 200);
-              }
-            };
-            return da.createDocs(db, opts);
-          }
-        };
-        return da.viewDocs(db, opts);
       }
-    };
-    return da.createDocs(db, opts);
-  },
-  // Retrieve a specific record or collection (as JSON)
-  getResource: function (req, res, next) {
-
-  },
-  // Retrieve a specific record in a specific format
-  viewRecord: function (req, res, next) {
-
-  },
-  // Retrieve all the records in a specific collection (as JSON)
-  getCollectionRecords: function (req, res, next) {
-
-  },
-  // Retrieve all the records in a specific collection in a specific format
-  viewCollectionRecords: function (req, res, next) {
-
-  },
-  // Update an existing record or collection
-  updateResource: function (req, res, next) {
-
-  },
-  // Delete a record or collection
-  deleteResource: function (req, res, next) {
-
-  },
-  // List files associated with a specific record
-  listFiles: function (req, res, next) {
-
-  },
-  // Associate a new file with a specific record
-  newFile: function (req, res, next) {
-
-  },
-  // Retrieve a specific file associated with a specific record
-  getFile: function (req, res, next) {
-
-  },
-  // Delete a specific file associated with a specific record
-  deleteFile: function (req, res, next) {
-
-  },
-  // List the schemas used by this application
-  listSchemas: function (req, res, next) {
-
-  },
-  // Get a specific schema used by this application
-  getSchema: function (req, res, next) {
-
+    })
   }
-};
+}
+
+// Upload an existing record
+function uploadRecord (req, res, next) {
+
+}
+
+// Put data in the database
+function saveRecord (req, res, next) {
+  var entries = req.entries
+    , db = mongo.getDb('harvest');
+  var opts = {
+    docs: entries,
+    error: function (err) {
+      return next(new errors.DatabaseWriteError(
+        'Error writing to the database'));
+    },
+    success: function (newHarvestDocs) {
+      var opts = {
+        design: 'input',
+        format: req.format,
+        keys: (function () {
+          var docIds = [];
+          _.each(newHarvestDocs, function (doc) {
+            docIds.push(doc.id);
+          });
+          return docIds;
+        })(),
+        error: function (err) {
+          return next(new errors.DatabaseReadError(
+            'Error reading document from database'));
+        },
+        success: function (transformedDocs) {
+          var db = mongo.getDb('record');
+          _.each(transformedDocs.row, function (doc) {
+            var harvestInfo = {
+              OriginalFormat: req.format,
+              HarvestURL: req.url,
+              HarvestDate: utils.getCurrentDate(),
+              HarvestRecordId: doc.id
+            };
+            _.extend(doc.value.HarvestInformation, harvestInfo);
+            _.extend(doc.value, {
+              Collections: req.collections || [],
+              ModifiedDate: utils.getCurrentDate()
+            })
+          });
+          var opts = {
+            docs: (function () {
+              var docVals = [];
+              _.each(transformedDocs.rows, function (doc) {
+                docVals.push(doc.value);
+              });
+              return docVals;
+            })(),
+            error: function (err) {
+              return next(new errors.DatabaseWriteError(
+                'Error writing to the database'));
+            },
+            success: function (newRecords) {
+              var resBody = (function () {
+                var results = [];
+                _.each(newRecords, function (rec) {
+                  results.push('/metadata/record/' + rec.id + '/');
+                });
+                return results;
+              })();
+              return res.send(resBody, 200);
+            }
+          };
+          return da.createDocs(db, opts);
+        }
+      };
+      return da.viewDocs(db, opts);
+    }
+  };
+  return da.createDocs(db, opts);
+}
+
+// Retrieve a specific record or collection (as JSON)
+function getResource (req, res, next) {
+
+}
+
+// Retrieve a specific record in a specific format
+function viewRecord (req, res, next) {
+
+}
+
+// Retrieve all the records in a specific collection (as JSON)
+function getCollectionRecords (req, res, next) {
+
+}
+
+// Retrieve all the records in a specific collection in a specific format
+function viewCollectionRecords (req, res, next) {
+
+}
+
+// Update an existing record or collection
+function updateResource (req, res, next) {
+
+}
+
+// Delete a record or collection
+function deleteResource (req, res, next) {
+
+}
+
+// List files associated with a specific record
+function listFiles (req, res, next) {
+
+}
+
+// Associate a new file with a specific record
+function newFile (req, res, next) {
+
+}
+
+// Retrieve a specific file associated with a specific record
+function getFile (req, res, next) {
+
+}
+
+// Delete a specific file associated with a specific record
+function deleteFile (req, res, next) {
+
+}
+
+// List the schemas used by this application
+function listSchemas (req, res, next) {
+
+}
+
+// Get a specific schema used by this application
+function getSchema (req, res, next) {
+
+}
+
+exports.search = search;
+exports.listResources = listResources;
+exports.viewRecords = viewRecords;
+exports.newResource = newResource;
+exports.harvestRecord = harvestRecord;
+exports.uploadRecord = uploadRecord;
+exports.saveRecord = saveRecord;
+exports.getResource = getResource;
+exports.viewRecord = viewRecord;
+exports.getCollectionRecords = getCollectionRecords;
+exports.viewCollectionRecords = viewCollectionRecords;
+exports.updateResource = updateResource;
+exports.deleteResource = deleteResource;
+exports.listFiles = listFiles;
+exports.newFile = newFile;
+exports.getFile = getFile;
+exports.deleteFile = deleteFile;
+exports.listSchemas = listSchemas;
+exports.getSchema = getSchema;
---
+++ src/utils.js
@@ -0,0 +1,128 @@
+var org = require('./organization-config')
+  , xml2json = require('xml2json');
+
+function atomWrapper (entries) {
+  return {
+    feed: {
+      xmlns: "http://www.w3.org/2005/Atom",
+      'xmlns:georss': "http://www.georss.org/georss",
+      'scast': "http://sciflo.jp;.nasa.gov/serviceCasting/2009v1",
+      id: {
+        $t: '' + org.orgUrl + '/resources/atom'
+      },
+      title: {
+        $t: '' + org.orgName + ' Atom Feed'
+      },
+      updated: {
+        $t: utils.getCurrentDate()
+      },
+      author: {
+        name: {
+          $t: org.orgName
+        },
+        email: {
+          $t: org.orgEmail
+        }
+      },
+      entries: {
+        entry: entries
+      }
+    }
+  }
+}
+
+function featureCollection (entries) {
+  return {
+    type: 'FeatureCollection',
+    features: entries
+  }
+}
+
+function getCurrentDate () {
+  function ISODateString (d) {
+    function pad (n) {
+      if (n < 0) return '0' + n;
+      return n;
+    }
+    return String((d.getUTCFullYear()) + '-' + (pad(d.getUTCMonth() + 1)) + '-'
+           + (pad(d.getUTCDate())) + 'T' + (pad(d.getUTCHours())) + ':'
+           + (pad(d.getUTCMinutes())) + ':' + (pad(d.getUTCSeconds())) + 'Z');
+  }
+  var now = new Date();
+  return ISODateString(now);
+}
+
+function validateHarvestFormat (format, data) {
+  if (format === 'csv') {
+    return true;
+  } else {
+    try {
+     var json = xml2json.toJson(data, {object: true, reversible: true});
+    } catch (err) {
+      return false
+    }
+    switch (format) {
+      case 'atom.xml':
+        if (json.feed) return true;
+        else return false;
+      case 'iso.xml':
+        if (json['gmd:MD_Metadata']) return true;
+        else return false;
+      case 'fgdc.xml':
+        if (json.metadata) return true;
+        else return false;
+    }
+  }
+}
+
+function addCollectionKeywords (iso, collectionNames) {
+  var outputKeywords = iso['gmd:MD_Metadata']['gmd:identificationInfo']
+    ['gmd:MD_DataIdentification']['gmd:descriptiveKeywords']
+    , collectionNames = (function () {
+      var results = [];
+      _.each(collectionNames, function (name) {
+        results.push({'gco:CharacterString': {'$t': name}});
+      })
+      return results;
+    })();
+  if (collectionNames.length > 0) {
+    var newKeywordBlock = {
+      "gmd:MD_Keywords": {
+        "gmd:keyword": collectionNames,
+        "gmd:thesaurusName": {
+          "xlink:href": "/metadata/collection/",
+          "gmd:CI_Citation": {
+            "gmd:title": {
+              "gco:CharacterString": {
+                "$t": "Server Collections"
+              }
+            },
+            "gmd:date": {
+              "gmd:CI_Date": {
+                "gmd:date": {
+                  "gco:Date": {
+                    "$t": "2012-06-06"
+                  }
+                },
+                "gmd:dateType": {
+                  "gmd:CI_DateTypeCode": {
+                    "codeList": "http://standards.iso.org/ittf/PubliclyAvailableStandards/ISO_19139_Schemas/resources/Codelist/gmxCodelists.xml#CI_DateTypeCode",
+                    "codeListValue": "publication"
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    };
+    outputKeywords.push(newKeywordBlock);
+  }
+  return iso;
+}
+
+exports.atomWrapper = atomWrapper;
+exports.featureCollection = featureCollection;
+exports.getCurrentDate = getCurrentDate;
+exports.validateHarvestFormat = validateHarvestFormat;
+exports.addCollectionKeywords = addCollectionKeywords;
